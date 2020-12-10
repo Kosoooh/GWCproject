dd1e5e6ccda394649fd1721e40ebffcad75a793b
Kosoooh <75784469+Kosoooh@users.noreply.github.com>
2020-12-10 13:08:05 +0200

    test

--- space.py
+++ space.py
@@ -1,83 +1,23 @@
-from flask import request
-from flask_restful import Resource
-from http import HTTPStatus
+from extensions import db
 
-from models.space import Space, space_list
+space_list = []
 
 
-class SpaceListResource(Resource):
-
-    def get(self):
-        data = []
-
-        for space in space_list:
-            if space.is_publish is True:
-                data.append(space.data)
-
-        return {'data': data}, HTTPStatus.OK
-
-    def post(self):
-        data = request.get_json()
-
-        space = Space(name=data['name'],
-                      reservations=data['reservations'])
-
-        space_list.append(space)
-
-        return space.data, HTTPStatus.CREATED
+def get_last_id():
+    if space_list:
+        last_space = space_list[-1]
+    else:
+        return 1
+    return last_space.id + 1
 
 
-class SpaceResource(Resource):
+class Space(db.Model):
+    __tablename__ = 'space'
 
-    def get(self, space_id):
-        space = next((space for space in space_list if space.id == space_id and space.is_publish == True), None)
-
-        if space is None:
-            return {'message': 'Workspace not found'}, HTTPStatus.NOT_FOUND
-
-        return space.data, HTTPStatus.OK
-
-    def put(self, space_id):
-        data = request.get_json()
-
-        space = next((space for space in space_list if space.id == space_id), None)
-
-        if space is None:
-            return {'message': 'Workspace not found'}, HTTPStatus.NOT_FOUND
-
-        space.name = data['name']
-
-        return space.data, HTTPStatus.OK
-
-    def delete(self, space_id):
-        space = next((space for space in space_list if space.id == space_id), None)
-
-        if space is None:
-            return {'message': 'Workspace not found'}, HTTPStatus.NOT_FOUND
-
-        space_list.remove(space)
-
-        return {}, HTTPStatus.NO_CONTENT
-
-
-class SpacePublishResource(Resource):
-
-    def put(self, space_id):
-        space = next((space for space in space_list if space.id == space_id), None)
-
-        if space is None:
-            return {'message': 'Workspace not found'}, HTTPStatus.NOT_FOUND
-
-        space.is_publish = True
-
-        return {}, HTTPStatus.NO_CONTENT
-
-    def delete(self, space_id):
-        space = next((space for space in space_list if space.id == space_id), None)
-
-        if space is None:
-            return {'message': 'Workspace not found'}, HTTPStatus.NOT_FOUND
-
-        space.is_publish = False
-
-        return {}, HTTPStatus.NO_CONTENT
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    reservations = db.Column(db.ARRAY(db.String()), nullable=True)
+    is_publish = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
