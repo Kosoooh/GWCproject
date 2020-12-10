dbaff72bbc0a614e09c5f51e2d77d7737cbad433
okoskne <olivear@outlook.com>
2020-12-10 14:03:32 +0200

    tst

--- models/space.py
+++ models/space.py
@@ -21,3 +21,4 @@ class Space(db.Model):
     created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
     updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
     user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
